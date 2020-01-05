fbe6a60826a9fc0b474e3a9ec9814a9e1c1a6336
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2020-01-05 07:51:06 +0700

    done test flow

---
+++ services/Mongoose.js
@@ -0,0 +1,30 @@
+const config = require('../config');
+const { host, user, pass, dbName, port } = config.mongodb;
+const { createUserRole } = require('../providers/UserRoleProvider');
+const mongoose = require('mongoose');
+
+function connectDatabase() {
+  let uri = `mongodb://${host}:${port}/${dbName}`;
+
+  options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    user,
+    pass
+  };
+
+  mongoose.connect(uri, options).then(rs => {
+    console.log('connect success to database: ', dbName);
+  });
+}
+
+function initUserRole() {
+  createUserRole({ name: 'Admin', value: 'admin' });
+  createUserRole({ name: 'Manager', value: 'manager' });
+}
+
+module.exports = {
+  connectDatabase
+};
