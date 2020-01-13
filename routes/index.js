43f965c9f89ca54eca74ca7fa3bcb2a38c70e69d
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2020-01-13 13:39:26 +0700

    remove index page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirroot, 'public', 'pages', 'index.html'));
-});
-
 router.get('/policy', (req, res) => {
   res.render('policy');
 });
