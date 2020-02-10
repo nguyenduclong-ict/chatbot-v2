67920c2af39f03b6ee6f8303baee973c3f7b0f4b
nguyenduclong-ict <nguyenduclong.ict@gmail.com>
2020-02-10 13:47:56 +0700

    fix 404 root path

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
+router.get('/', (req, res) => {
+  res.send('Chatbot ichina');
+});
+
 router.get('/policy', (req, res) => {
   res.render('policy');
 });
